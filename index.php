c29a52e7ddd8d86d2c921e87f8cabb49f01c2042
Wellmac <wellmac777@gmail.com>
2020-04-15 14:55:58 -0300

    criamos a classe Usuario

--- index.php
+++ index.php
@@ -3,10 +3,17 @@
 
 require_once("config.php");
 
+$root = new Usuario();
+
+$root->loadbyId(11);
+
+echo $root;
+
+/*
 $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario >= 7");
 
 echo json_encode($usuarios);
-
+*/
 ?>
